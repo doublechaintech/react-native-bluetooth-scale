904e8f004ebcc2fc96b11ef6fefbd6e71b671ba2
Philip Z <philip_chang@163.com>
2023-06-09 09:28:24 +0000

    share

---
+++ init.sh
@@ -0,0 +1,8 @@
+echo "# react-native-bluetooth-scale" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/doublechaintech/react-native-bluetooth-scale.git
+git push -u origin master
+
